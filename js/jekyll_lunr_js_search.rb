319899f975ce91f403724532a3f967c0cb3ac329
Lidewey <lidewey.vanderwal@gmail.com>
2014-08-08 14:16:36 +0200

    Change back js file

--- js/jekyll_lunr_js_search.rb
+++ js/jekyll_lunr_js_search.rb
@@ -172,36 +172,3 @@ module Jekyll
   end
   
 end
-
-<!-- Add an element to contain the list of search result entries. -->
-<section id="search-results" style="display: none;">
-  <p>Search results</p>
-  <div class="entries">
-  </div>
-</section>
-
-<!-- Create a Mustache template to display the search results. -->
-{% raw %}
-<script id="search-results-template" type="text/mustache">
-  {{#entries}}
-    <article>
-      <h3>
-        {{#date}}<small><time datetime="{{pubdate}}" pubdate>{{displaydate}}</time></small>{{/date}}
-        <a href="{{url}}">{{title}}</a>
-      </h3>
-    </article>
-  {{/entries}}
-</script>
-{% endraw %}
-
-<!-- Configure the jQuery plugin for the search input field. -->
-<script type="text/javascript">
-  $(function() {
-    $('#search-query').lunrSearch({
-      indexUrl: '/search.json',             // URL of the `search.json` index data for your site
-      results:  '#search-results',          // jQuery selector for the search results container
-      entries:  '.entries',                 // jQuery selector for the element to contain the results list, must be a child of the results element above.
-      template: '#search-results-template'  // jQuery selector for the Mustache.js template
-    });
-  });
-</script>
