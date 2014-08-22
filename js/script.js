1c3c72e4f70259410d4e386f4428d3570a5c07c0
Lidewey <lidewey.vanderwal@gmail.com>
2014-08-22 14:40:36 +0200

    Active menu bar

--- js/script.js
+++ js/script.js
@@ -21,8 +21,8 @@ $(function () {
     }
 });
 
-l=$("a[href*='" + location.pathname + "']").addClass("current");
-$( "l" ).addClass( "current" );
+var l=$("a[href*='" + location.pathname + "']").addClass("current");
+l.addClass( "current" );
 
 
 
